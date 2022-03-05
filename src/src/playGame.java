b1fbd1c8445e154d94db3473c882688195c75eeb
Aarohan Mishra <am9705@rit.edu>
2022-03-05 14:47:37 -0500

    Created new interface and made change to playGame

--- src/src/playGame.java
+++ src/src/playGame.java
@@ -1,10 +1,14 @@
 package src;
+
+import java.util.ArrayList;
+
 /**
  * File name: playGame.java
  * @author: Aarohan Mishra and Shimanto Bhowmik
  */
 public class playGame {
+    ArrayList<NPCTeam> teams= new ArrayList<>();
     public playGame(){
-        
+
     }
 }
