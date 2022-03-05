c91b9b17da853f320ffd53a631da954167f9e0da
Shimanto Bhowmik <sb6778@g.rit.edu>
2022-03-05 16:04:08 -0500

    Team interface implemented

--- src/src/Team.java
+++ src/src/Team.java
@@ -1,20 +1,15 @@
 package src;
 
-import java.util.Comparator;
+
 
 /**
  * File Name: TeamComparator.java
  * @author: Shimanto Bhowmik
  */
-public class Team implements Comparator<myTeam> {
+public interface Team  {
 
-    @Override
-    public int compare(myTeam first, myTeam second) {
-        int result = first.getTotalPoints() - second.getTotalPoints();
-        if(result ==0){
-            result = first.getTeamName().compareTo(second.getTeamName());
-        }
-        return result;
+    public int getPoints();
+    public String getName();
     }
     
-}
+
