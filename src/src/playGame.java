c75063e0fd74ba16ff9acb81a499f805d4db6418
Aarohan Mishra <am9705@rit.edu>
2022-03-05 15:26:53 -0500

    Made changes

--- src/src/playGame.java
+++ src/src/playGame.java
@@ -13,19 +13,27 @@ public class playGame {
     public playGame()throws IOException{
         teams= new ArrayList<>();
         r= new Random();
-        BufferedReader fr1= new BufferedReader(new FileReader("AnimalName.txt"));
         ArrayList<String> animalNames= new ArrayList<>();
         ArrayList<String> attributes= new ArrayList<>();
+        try{
+        BufferedReader fr1= new BufferedReader(new FileReader("AnimalName.txt"));
         String s1= fr1.readLine();
         while(s1!=null){
             animalNames.add(s1);
             s1= fr1.readLine();
         }
+        }catch(IOException E){
+            System.err.println("File not found");
+        }
+        try{
         BufferedReader fr2= new BufferedReader(new FileReader("Attributes.txt"));
         String s2= fr2.readLine();
         while(s2!= null){
             attributes.add(s2);
             s2= fr2.readLine();
+        }
+        }catch(IOException E){
+            System.err.println("File not found");
         }
         int i;
         for(i=0;i<9;i++){
@@ -37,8 +45,6 @@ public class playGame {
             NPCTeam e= new NPCTeam(name);
             teams.add(e);
         }
-        fr1.close();
-        fr2.close();
     }
     public void displayTeams(){
         for(NPCTeam E: teams){
